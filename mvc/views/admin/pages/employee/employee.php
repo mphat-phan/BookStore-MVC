484ca5a7b488de200e08a447337e27152cfca2f6
mphat-phan <62046658+mphat-phan@users.noreply.github.com>
2021-10-23 00:06:04 +0700

    Hoàn tất giao diện

--- mvc/views/admin/pages/employee/employee.php
+++ mvc/views/admin/pages/employee/employee.php
@@ -9,7 +9,7 @@
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Sell</li>
+                        <li class="breadcrumb-item active">Quản lý nhân viên</li>
                     </ol>
                 </div>
             </div>
@@ -25,7 +25,7 @@
 
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">Sell table</h3>
+                            <h3 class="card-title">Employee table</h3>
 
                             <button type="button" onclick="" href="#" class="btn btn-primary btn-sm float-right" role="button"
                                 data-toggle="modal" data-target="#AddModal">Add</button>
@@ -36,7 +36,7 @@
 
                         <!-- /.card-header -->
                         <div class="card-body">
-                            <table id="authortable" class="table table-bordered table-striped">
+                            <table id="employeetable" class="table table-bordered table-striped">
                                 <thead>
                                     <tr>
                                         <th>Rendering engine</th>
