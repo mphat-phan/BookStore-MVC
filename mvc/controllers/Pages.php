2cd8e7795b76e8b8cd410149734b3bdebd2420dc
DoanChiQuang <chiquang127@gmail.com>
2021-10-24 20:40:31 +0700

    page404

--- mvc/controllers/Pages.php
+++ mvc/controllers/Pages.php
@@ -2,7 +2,11 @@
 class Pages extends Controller{
     function __construct(){
         $this->view("pages/404");
+        
     }
     
 }
 ?>
+<script>
+window.history.pushState("", "404", "404.php");
+</script>
--- mvc/views/layout.php
+++ mvc/views/layout.php
@@ -1,7 +1,7 @@
 <?php
   // khác port => http://localhost/Bookstore/
   // có port => http://localhost:(PORT)/Bookstore/
-  define ('URL', 'http://localhost/Bookstore/');
+  define ('URL', 'http://localhost:88/Bookstore/');
 ?>
 <!DOCTYPE html>
 <html lang="en">
